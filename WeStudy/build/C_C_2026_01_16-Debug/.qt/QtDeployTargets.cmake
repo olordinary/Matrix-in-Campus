a45875fa8312536a756a77780bdac831cb14a78c
王欣如 <2742138496@qq.com>
2026-01-16 22:57:27 +0800

    任务分工视频(改变）

--- WeStudy/build/C_C_2026_01_16-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appWeStudy_FILE /root/Matrix-in-Campus/WeStudy/build/C_C_2026_01_16-Debug/appWeStudy)
-set(__QT_DEPLOY_TARGET_appWeStudy_TYPE EXECUTABLE)
--- WeStudy/build/C_C_2026_01_16-Debug/.qt/appWeStudy_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appWeStudy")
-set(working_dir "/root/Matrix-in-Campus/WeStudy")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/root/Matrix-in-Campus/WeStudy/build/C_C_2026_01_16-Debug/.qt/appWeStudy_res.txt")
